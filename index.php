c355e95746b28425a9b27f4511e20316ce44fdc8
YosephCode <yosephcsantos@gmail.com>
2014-06-06 02:21:10 -0300

    subindo arquivos

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width">
+		<meta http-equiv="X-UA-COmpatible" content="IE-edge, chrome=1">
+		<title></title>
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	<body>
+		<header>
+			<input type="search" class="search">
+			<img src="img/ycode.png" class="logo">
+		</header>
+		<section class="perfil">
+			
+			
+		</section>
+
+		<footer>
+			<nav>
+				<ul>
+					<li><a href="#" class="icon-tumblr"><span class="mls"></span>Twitter</a></li>
+					<li><a href="#" class="icon-linkedin"><span class="mls"></span>Linkedin</a></li>
+					<li><a href="#" class="icon-github"><span class="mls"></span>GitHub</a></li>
+				</ul>
+			</nav>
+		</footer>
+	</body>
+</html>
