003a5d0cc5701a5ac269e93f858b9f640c0393bc
YosephCode <yosephcsantos@gmail.com>
2014-06-12 02:18:07 -0300

    alteracoes sem responsividade

--- index.php
+++ index.php
@@ -8,16 +8,103 @@
 		<link rel="stylesheet" href="css/main.css">
 	</head>
 	<body>
+
 		<header>
-			<input type="search" class="search">
+			<input type="search" class="search" value="">
 			<img src="img/ycode.png" class="logo">
 		</header>
-		<section class="perfil">
+
+		<section class="principal">
+			<section class="conteudo">
+				
+				<div class="bandeirinha">		
+				</div>
 			
-			
+				<p class="frase"><span class="espaco"></span>Aqui vai ficar um texto bem pequeno com 
+					dizeres meu, algo que chame atenção, 
+					marcante!<span class="espaco"></span>
+				</p>				
+			</section>
+				<hr>
+				<hr>
+				<hr>
+			<section class="habilidades">
+				<article class="front">
+					<h2>Front-End:</h2>
+						<ul>
+							<li>HTML5</li> 
+							<li>CSS3</li>
+							<li>JAVASCRIPT</li>
+							<li>Responsive Web Design</li>
+						</ul>
+				</article>
+
+				<article class="back">
+					<h2>Back-End:</h2>
+					<ul>
+						<li>PHP</li> 
+						<li>SQL</li>
+						<li>JAVA</li>
+						<li>C</li>
+					</ul>
+				</article>
+
+				<article class="editor">
+					<h2>Editores de Códigos:</h2>
+					<ul>
+						<li>Sublime Text</li> 
+						<li>Netbeans</li>
+						<li>Eclipse</li>
+						<li>Notepad++</li>
+					</ul>
+					<br><br><br>
+					<hr>
+					<br><br>
+				
+					<h2>Editores de Gráficos:</h2>
+					<ul>
+						<li>Photoshop</li> 
+						<li>Fireworks</li>
+						<li>Illustrator</li>
+						<li>InDesign</li>
+					</ul>
+				</article>			
+			</section>
+
+
+			<section>
+				<article class="imagem">
+					<img src="img/img.jpg" class="perfil-foto">
+				</article>
+				<p class="frase2">
+					<span class="espaco"></span>
+					Aqui vai ficar um texto bem pequeno com 
+					dizeres meu, algo que chame atenção, 
+					marcante!<span class="espaco"></span>
+				</p>
+			</section>
+
 		</section>
 
-		<footer>
+		<section class="outros">
+			<article class="sistemasOP">
+			<h2>Sistemas Operacionais</h2>
+			<ul>
+				<li>Linux - Ubuntu</li>				
+				<li>Windows Seven</li>
+			</ul>
+			</article>
+			<article class="cursos">
+				<h2>Cursos Complementares</h2>
+				<ul>
+				<li>Formação Front-End na CAELUM</li>				
+				<li>Computação Gráfica na Seven</li>
+			</ul>
+			</article>
+		</section>
+
+
+		<footer class="footer">
 			<nav>
 				<ul>
 					<li><a href="#" class="icon-tumblr"><span class="mls"></span>Twitter</a></li>
@@ -26,5 +113,7 @@
 				</ul>
 			</nav>
 		</footer>
+
+
 	</body>
 </html>
