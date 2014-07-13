5e105fc8bc7301862bfbed140838f363f01eeebb
YosephCode <yosephcsantos@gmail.com>
2014-07-13 20:47:15 -0300

    mudando background com js

--- js/button.js
+++ js/button.js
@@ -3,9 +3,12 @@ var esconde = "Cursos";
 var mostra = "Portifolio";
 
 var some = document.querySelector(".button");
-var hab = document.querySelector(".habilidades");
-
+var hab = document.querySelector(".habilidades ");
+var ft = document.querySelector(".imagem");
+var hd = some.parentNode;
 some.addEventListener("click", function(event){
+	hd.classList.toggle("header");/*style.backgroundColor.toggle="rgba(228,239,192,0.7)";*/
+	ft.classList.toggle("invisivel")
 	hab.classList.toggle("invisivel");
 	if(hab.classList.contains("invisivel")){
 		this.textContent = mostra;
