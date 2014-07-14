ac7962dbf8feb2c19ddaf17a584c5e23c727edb3
YosephCode <yosephcsantos@gmail.com>
2014-07-13 21:15:46 -0300

    organizando codg js

--- js/button.js
+++ js/button.js
@@ -1,18 +1,2 @@
-
-var esconde = "Cursos";
-var mostra = "Portifolio";
-
-var some = document.querySelector(".button");
-var hab = document.querySelector(".habilidades ");
-var ft = document.querySelector(".imagem");
-var hd = some.parentNode;
-some.addEventListener("click", function(event){
-	hd.classList.toggle("header");/*style.backgroundColor.toggle="rgba(228,239,192,0.7)";*/
-	ft.classList.toggle("invisivel")
-	hab.classList.toggle("invisivel");
-	if(hab.classList.contains("invisivel")){
-		this.textContent = mostra;
-	}else{
-		this.textContent = esconde;
-	}
-});
+var esconde = "Cursos";var mostra = "Portifolio";var some = document.querySelector(".button");var hab = document.querySelector(".habilidades ");var ft = document.querySelector(".imagem");
+var hd = some.parentNode;some.addEventListener("click", function(event){hd.classList.toggle("header");ft.classList.toggle("invisivel");hab.classList.toggle("invisivel");if(hab.classList.contains("invisivel")){this.textContent = mostra;}else{this.textContent = esconde;}});
