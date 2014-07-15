a080b651712babbdc994411b5e277547d632e48a
Yoseph Santos <yosephcsantos@gmail.com>
2014-07-15 20:20:35 -0300

    tempo da animacao button

--- js/button.js
+++ js/button.js
@@ -1,2 +1,2 @@
-var hab = $(".habilidades, .imagem");var hd = $("header");var bt= $(".button").text("Contato");bt.click(function(){hab.slideToggle(1000, function(){if(hab.css("display") == "none"){bt.text("Portifólio");hd.addClass("header");}else{bt.text("Contato");hd.removeClass("header");}});});
+var hab = $(".habilidades, .imagem");var hd = $("header");var bt= $(".button").text("Contato");bt.click(function(){hab.slideToggle(400, function(){if(hab.css("display") == "none"){bt.text("Portifólio");hd.addClass("header");}else{bt.text("Contato");hd.removeClass("header");}});});
 
