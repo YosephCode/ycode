0bbdd010731c687b2e82feb5a6206cc259ad7791
Yoseph Santos <yosephcsantos@gmail.com>
2014-07-17 20:10:44 -0300

    Bem vindo Boilerplate

--- index.php
+++ index.php
@@ -1,116 +1,145 @@
-<!doctype html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width">
-		<title>YosephCode</title>
-		<link rel="stylesheet" href="css/main.css">
-	</head>
-	<body>
-		<section class="principal">
-			<header>
-				<button class="button"></button>	
-				<h1>Ycode</h1>
-			</header>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-			<article class="conteudo">
-				<article class="eu">
-					<div class="imagem">
-						<img src="img/img.jpg" alt="minha-foto" class="perfil-foto">
-						<p class="frase2">
-							<span class="espaco"></span>
-								I'm web development enthusiast and I work on the open web!
-							<span class="espaco"></span>
-						</p>
-					</div>
-				</article>
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-				<article class="habilidades">
-					<div class="front">
-						<h2>Front-End:</h2>
-						<ul class="itens">
-							<li>Html5</li> 
-							<li>Css3</li>
-							<li>Javascript</li>
-							<li>Responsive Web Design</li>
-						</ul>
-					</div>
-					<div class="back">
-						<h2>Back-End:</h2>
-						<ul class="itens">
-							<li>Php</li> 
-							<li>Sql</li>
-							<li>Java</li>
-							<li>C</li>
-						</ul>
-					</div>
-					<div class="editorCodigo">
-						<h2>Editors Codes:</h2>
-						<ul class="itens">
-							<li>Sublime Text</li> 
-							<li>Netbeans</li>
-							<li>Eclipse</li>
-							<li>Notepad++</li>
-						</ul>
-					</div>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
 
-					<div class="editorGrafico">
-						<h2>Graphics editors:</h2>
-						<ul class="itens">
-							<li>Photoshop</li> 
-							<li>Fireworks</li>
-							<li>Illustrator</li>
-							<li>InDesign</li>
-						</ul>
-					</div>
-				</article>
+        <!-- Add your site or application content here -->
+       <section class="principal">
+            <header>
+                <button class="no-js butt">Contato</button> 
+                <h1>Ycode</h1>
+            </header>
 
-				<article class="form">
-					<form>
-						<h2 class="label">Talk with me!</h2>
-							<label>Name</label>
-							<input type="text" class="nome" required>
-							<label>Email</label>
-							<input type="email" class="email" required>
-							<label>Message</label>
-							<textarea class="textearea" required></textarea>
-							
-							<input type="submit" name="Submit"  class="sbmt" value="Submit" />
-					</form>	
-				</article>
-				
-				<article class="complementares">
-					<div class="cursos">
-						<h2 class="h2complementares">Additional Courses</h2>
-						<ul class="lidaul">
-							<li>Web with Html, Css and Javascript (CAELUM)</li>
-							<li>Front-end with Javascript and jQuery (CAELUM)</li>
-							<li>Computer Graphics (Seven)</li>
-						</ul>
-					</div>
-					<div class="sistemasOP">
-						<h2 class="h2complementares">Operating Systems</h2>
-						<ul class="lidaul">
-							<li>Linux - Ubuntu</li>				
-							<li>Windows - Seven</li>
-							<li>Mac - OS X Mavericks</li>
-						</ul>
-					</div>
+            <article class="conteudo">
+                <article class="eu">
+                    <div class="imagem">
+                        <img src="img/img.jpg" alt="minha-foto" class="perfil-foto">
+                        <p class="frase2">
+                            <span class="espaco"></span>
+                                I'm web development enthusiast and I work on the open web!
+                            <span class="espaco"></span>
+                        </p>
+                    </div>
+                </article>
 
-				</article>
-			</article>
-			
-			<footer class="footer">
-				<nav>
-					<ul>
-						<li><a href="https://twitter.com/Yoseph_Santos" class="icon-tumblr"><span class="mls"></span>Twitter</a></li>
-						<li><a href="https://br.linkedin.com/pub/yoseph-santos/38/67b/b65/" class="icon-linkedin"><span class="mls"></span>Linkedin</a></li>
-						<li><a href="https://github.com/YosephCode" class="icon-github"><span class="mls"></span>GitHub</a></li>
-					</ul>
-				</nav>
-			</footer>
-		</section>
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<script src="js/button.js"></script>
-	</body>
+                <article class="habilidades">
+                    <div class="front">
+                        <h2>Front-End:</h2>
+                        <ul class="itens">
+                            <li>Html5</li> 
+                            <li>Css3</li>
+                            <li>Javascript</li>
+                            <li>Responsive Web Design</li>
+                        </ul>
+                    </div>
+                    <div class="back">
+                        <h2>Back-End:</h2>
+                        <ul class="itens">
+                            <li>Php</li> 
+                            <li>Sql</li>
+                            <li>Java</li>
+                            <li>C</li>
+                        </ul>
+                    </div>
+                    <div class="editorCodigo">
+                        <h2>Editors Codes:</h2>
+                        <ul class="itens">
+                            <li>Sublime Text</li> 
+                            <li>Netbeans</li>
+                            <li>Eclipse</li>
+                            <li>Notepad++</li>
+                        </ul>
+                    </div>
+
+                    <div class="editorGrafico">
+                        <h2>Graphics editors:</h2>
+                        <ul class="itens">
+                            <li>Photoshop</li> 
+                            <li>Fireworks</li>
+                            <li>Illustrator</li>
+                            <li>InDesign</li>
+                        </ul>
+                    </div>
+                </article>
+
+                <article class="form">
+                    <form>
+                        <h2 class="label">Talk with me!</h2>
+                            <label>Name</label>
+                            <input type="text" class="nome" required>
+                            <label>Email</label>
+                            <input type="email" class="email" required>
+                            <label>Message</label>
+                            <textarea class="textearea" required></textarea>
+                            
+                            <input type="submit" name="Submit"  class="sbmt" value="Submit" />
+                    </form> 
+                </article>
+                
+                <article class="complementares">
+                    <div class="cursos">
+                        <h2 class="h2complementares">Additional Courses</h2>
+                        <ul class="lidaul">
+                            <li>Web with Html, Css and Javascript (CAELUM)</li>
+                            <li>Front-end with Javascript and jQuery (CAELUM)</li>
+                            <li>Computer Graphics (Seven)</li>
+                        </ul>
+                    </div>
+                    <div class="sistemasOP">
+                        <h2 class="h2complementares">Operating Systems</h2>
+                        <ul class="lidaul">
+                            <li>Linux - Ubuntu</li>             
+                            <li>Windows - Seven</li>
+                            <li>Mac - OS X Mavericks</li>
+                        </ul>
+                    </div>
+
+                </article>
+            </article>
+            
+            <footer class="footer">
+                <nav>
+                    <ul>
+                        <li><a href="https://twitter.com/Yoseph_Santos" class="icon-tumblr"><span class="mls"></span>Twitter</a></li>
+                        <li><a href="https://br.linkedin.com/pub/yoseph-santos/38/67b/b65/" class="icon-linkedin"><span class="mls"></span>Linkedin</a></li>
+                        <li><a href="https://github.com/YosephCode" class="icon-github"><span class="mls"></span>GitHub</a></li>
+                    </ul>
+                </nav>
+            </footer>
+        </section>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script src="js/button.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
 </html>
--- js/button.js
+++ js/button.js
@@ -1,4 +1,4 @@
 /*Button*/
-var hab = $(".habilidades, .imagem");var hd = $("header");var bt= $(".button").text("Contato");bt.click(function(){hab.slideToggle(400, function(){if(hab.css("display") == "none"){bt.text("Portfolio");hd.addClass("header");}else{bt.text("Contact");hd.removeClass("header");}});});
+var hab = $(".habilidades, .imagem");var hd = $("header");var bt= $(".butt").text("Contato");bt.click(function(){hab.slideToggle(400, function(){if(hab.css("display") == "none"){bt.text("Portfolio");hd.addClass("header");}else{bt.text("Contact");hd.removeClass("header");}});});
 /*texteare*/
 $("textarea").bind("input", function(e) {while($(this).outerHeight()<this.scrollHeight+parseFloat($(this).css("borderTopWidth"))+parseFloat($(this).css("borderBottomWidth"))&& $(this).height() < 500){$(this).height($(this).height()+1);};});
