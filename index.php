b4fd09a173e7b40a7afb3adf459ff9db9baac18a
Yoseph Santos <yosephcsantos@gmail.com>
2014-07-25 19:05:52 -0300

    concateneni o css e o js

--- index.php
+++ index.php
@@ -7,11 +7,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Yoseph Code</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link rel="stylesheet" href="css/animate.css">
-        <link rel="stylesheet" href="css/normalize.css">
+        <meta name="description" content=""><meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/main.css">
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
@@ -122,9 +118,7 @@
                 </nav>
             </footer>
         </section>
-
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
     </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -3,5 +3,11 @@ var hab = $(".habilidades, .imagem");var hd = $("header");var bt= $(".butt").tex
 /*texteare*/
 $("textarea").bind("input", function(e) {while($(this).outerHeight()<this.scrollHeight+parseFloat($(this).css("borderTopWidth"))+parseFloat($(this).css("borderBottomWidth"))&& $(this).height() < 500){$(this).height($(this).height()+1);};});
 if(Modernizr.touch){/*Sem-animate-no-mobile*/}else{$(function(){var but = $(".butt");but.addClass("animated bounceInRight");$("h1").addClass("animated fadeInDown");$("header").addClass("animated shake");$(".frase2").addClass("animated tada");$(".perfil-foto").addClass("animated wobble");$(".front, .back").addClass("animated pulse");});}
+// Avoid `console` errors in browsers that lack a console.
+(function(){var method;var noop = function () {};var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error','exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log','markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+'timeStamp', 'trace', 'warn'];var length = methods.length;var console = (window.console = window.console || {});while (length--){method = methods[length];
+if (!console[method]) {console[method] = noop;}}}());
+
+
 
 
