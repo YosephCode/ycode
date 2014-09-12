89b475b431ddfe0618ea7944b6bb87d962f23822
Yoseph Code <yosephcsantos@gmail.com>
2014-09-12 15:54:20 -0300

    lang=en no html

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html xmlns:ng="http://angularjs.org" id="ng-app" ng-app="comentario" class="no-js lt-ie9 "> <![endif]-->
-<!--[if gt IE 8]><!--> <html ng-app="comentario" class="no-js"> <!--<![endif]-->
+<!--[if IE 8]>         <html xmlns:ng="http://angularjs.org" id="ng-app" ng-app="comentario" class="no-js lt-ie9 " lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html ng-app="comentario" class="no-js" lang="en"> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
